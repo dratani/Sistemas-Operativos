6237f2a1c1f807e7065f8a36d196ec125bd0dd44
tani <dratani@gmail.com>
2022-11-30 12:04:26 -0600

    Wait

---
+++ Procesos/ejercicioWait.c
@@ -0,0 +1,35 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+  pid_t pid;
+  int status;
+  pid = fork();
+  if(pid==0)
+    {
+      printf("[H1] Dormiré 5 seg\n");
+      sleep(5);
+      exit(5);
+    }
+    else
+    {
+      printf("[P] Esperaré al H1\n");
+      while(pid != wait(&status));
+    }
+  pid = fork();
+  if(pid== 0)
+  {
+    printf("[H2] Dormiré 1 seg\n");
+    sleep(1);
+    exit(1);
+  }
+  else
+  {
+    printf("[Padre] Esperaré al H2\n");
+    while(pid != wait(&status));
+  }
+}
