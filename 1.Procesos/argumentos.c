b796463d4797216f64fe0fe0df8a8727476b9b90
tani <dratani@gmail.com>
2023-04-04 13:15:17 -0600

    borrar

--- 1.Procesos/argumentos.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-int main (int argc, char *argv[]){
-printf("Programa secundario\n");
-for (int i=0;i<argc;i++){
-  printf("Argumento %d:%s\n",i,argv[i]);
-}
-_exit(0);
-}
