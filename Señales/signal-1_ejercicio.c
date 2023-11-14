448ebc804b77274bd0c356fb49b28307923d9dc3
tani <dratani@gmail.com>
2023-11-14 11:53:11 -0600

    una función de captura

--- Señales/signal-1_ejercicio.c
+++ Señales/signal-1_ejercicio.c
@@ -4,16 +4,21 @@
 #include <stdlib.h>
 struct sigaction capturadorSenial;
 int i;
-void capturaSigInt (int sig) {
-  printf ("Capturada la senial SIGINT con el número %d\n",sig);
-  exit(-1);
+void captura (int sig) {
+  if (sig==2)
+    printf ("Capturada la senial SIGINT con el número %d\n",sig);
+else{
+
+}
+//  exit(-1);
 }
 int main(void) {
-  capturadorSenial.sa_handler = capturaSigInt;
+  capturadorSenial.sa_handler = captura;
   sigaction (SIGINT, &capturadorSenial, NULL);
+  sigaction (SIGFPE, &capturadorSenial, NULL);
 
   while (1){
       printf ("En espera de Ctrl-C \n");
-      sleep(999);
+      sleep(10);
   }
 }
