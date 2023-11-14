ad4c008f8a75d7bbb246a9d7bc4f0e434a5a7bb6
tani <dratani@gmail.com>
2023-11-14 11:48:55 -0600

    pid correcto, proceso padre

--- Señales/kill.c
+++ Señales/kill.c
@@ -3,17 +3,19 @@
 #include <signal.h>
 #include <stdlib.h>
 int main(void){
-	int pid;
-	if (pid = fork() ==0){
+	int pid = fork();
+	if (pid==0){
 		/* código de proceso hijo*/
 		while (1) {
 			/* code */
 			printf("Hijo. PID=%d \n",getpid());
-			sleep(1);
+			sleep(2);
 		}
 	}
-	sleep(10);
-	printf("Padre. Terminación del proceso %d  \n", pid);
-	kill (pid,SIGTERM);
-	exit(0);
+	else{
+		sleep(10);
+		printf("Padre %d. Terminación del proceso %d  \n", getpid(), pid);
+		kill (pid,SIGTERM);
+		exit(0);
+	}
 }
